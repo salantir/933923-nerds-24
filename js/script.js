414414f4e98e8826f5b9250bc33c346d9c3f1295
Alexandr <salantir@mail.ru>
2019-03-27 23:21:14 +0300

    js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
       writeus.addEventListener("click", function (evt) {
         evt.preventDefault();
         popup.classList.add("modal-show");
+		popup.classList.remove("modal-error");
 		login.focus();
       });
 
@@ -17,4 +18,15 @@
 	  
 	  form.addEventListener("submit", function(evt) {
 		 evt.preventDefault();
+		 popup.classList.add("modal-error");
+	  });
+	  
+	  window.addEventListener("keydown", function (evt) {
+		if (evt.keyCode === 27) {
+			evt.preventDefault();
+		if (popup.classList.contains("modal-show")) {
+			popup.classList.remove("modal-show");
+			popup.classList.remove("modal-error");
+		  }	
+		}
 	  });
